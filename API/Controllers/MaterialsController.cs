040792135c21c7cce26a716d1310b22139788e9c
totskaiasonia <totskaiasonia@gmail.com>
2024-05-30 08:05:36 +0300

    add meterials upload

---
+++ API/Controllers/MaterialsController.cs
@@ -0,0 +1,63 @@
+﻿using Azure.Data.Tables;
+using Azure.Storage.Blobs;
+using Azure.Storage.Queues;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Reflection.Metadata;
+
+namespace API.Controllers
+{
+    [Route("api/materials")]
+    [ApiController]
+    public class MaterialsController : ControllerBase
+    {
+        private string _connectionString = "AccountName=devstoreaccount1;AccountKey=Eby8vdM02xNOcqFlqUwJPLlmEtlCDXJ1OUzFT50uSRZ6IFsuFq2UVErCz4I6tq/K1SZFPTOtr/KBHBeksoGMGw==;DefaultEndpointsProtocol=http;BlobEndpoint=http://127.0.0.1:10000/devstoreaccount1;QueueEndpoint=http://127.0.0.1:10001/devstoreaccount1;TableEndpoint=http://127.0.0.1:10002/devstoreaccount1;";
+        private string _tableName = "events";
+
+        private TableClient _tableClient;
+        private string _containerName = "materials";
+        
+
+        public MaterialsController()
+        {
+            _tableClient = new TableClient(_connectionString, _tableName);
+            _tableClient.CreateIfNotExists();
+
+
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Post(IFormFile file)
+        {
+            if (file == null || file.Length == 0)
+                return BadRequest("No file uploaded");
+
+            try
+            {
+                BlobServiceClient blobServiceClient = new BlobServiceClient(_connectionString);
+                BlobContainerClient blobContainerClient = blobServiceClient.GetBlobContainerClient(_containerName);
+
+                await blobContainerClient.CreateIfNotExistsAsync();
+
+                string blobName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+
+                BlobClient blobClient = blobContainerClient.GetBlobClient(blobName);
+
+                using (var stream = file.OpenReadStream())
+                {
+                    await blobClient.UploadAsync(stream, overwrite: true);
+                }
+
+                return Ok( new
+                {
+                    url = blobClient.Uri,
+                });
+            }
+            catch (Exception err)
+            {
+                return StatusCode(StatusCodes.Status500InternalServerError, $"Internal server error: {err.Message}");
+            }
+        }
+    }
+}
